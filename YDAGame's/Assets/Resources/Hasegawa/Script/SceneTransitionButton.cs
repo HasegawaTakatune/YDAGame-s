0a5d3dab9e135f0226fe568aa1ed75e89dc7a42e
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-05-31 13:26:28 +0900

    シーン移動ボタン事前作成

---
+++ YDAGame's/Assets/Resources/Hasegawa/Script/SceneTransitionButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransitionButton : MonoBehaviour {
+
+	[SerializeField] private string loadSceneName;
+	private Button button;
+
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		button.onClick.AddListener (OnClickButton);
+	}
+
+	private void OnClickButton(){
+		SceneManager.LoadScene (loadSceneName);
+	}
+}
