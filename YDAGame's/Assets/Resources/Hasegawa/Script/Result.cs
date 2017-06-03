acc221a37bd00723607c2d01c1a4c8c0da6b27fd
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-06-03 20:00:00 +0900

    リザルト画面の作成

--- YDAGame's/Assets/Resources/Hasegawa/Script/Result.cs
+++ YDAGame's/Assets/Resources/Hasegawa/Script/Result.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class Result : MonoBehaviour {
 
-	[SerializeField]private Text scoreText;
+	private Text Score;
+	private Text Rank;
+	private Text HitRate;
 	private int count = 0;
 	private int score;
 
 	// Use this for initialization
 	void Start () {
+		Score = GameObject.Find ("Score").GetComponent<Text> ();
+		Rank = GameObject.Find ("Rank").GetComponent<Text> ();
+		HitRate = GameObject.Find ("HitRate").GetComponent<Text> ();
 		score = GameStatus.GetScore ();
 		StartCoroutine ("ScoreCount");
 	}
@@ -18,9 +23,25 @@ public class Result : MonoBehaviour {
 	IEnumerator ScoreCount(){
 		while (true) {
 			count++;
-			scoreText.text = "Score : " + count;
+			Score.text = count.ToString ();
 			yield return new WaitForSeconds (0.01f);
 			if (count >= score)	break;
 		}
+
+		if (score >= 1000) {
+			Rank.text = "S";
+		} else if (score < 1000 && score >= 900) {
+			Rank.text = "A";
+		} else if (score < 900 && score >= 800) {
+			Rank.text = "B";
+		} else if (score < 800 && score >= 700) {
+			Rank.text = "C";
+		} else if (score <= 700) {
+			Rank.text = "D";
+		}
+
+		int hitRate = GameStatus.GetHitRate ();
+		HitRate.text = hitRate.ToString () + "%";
+
 	}
 }
