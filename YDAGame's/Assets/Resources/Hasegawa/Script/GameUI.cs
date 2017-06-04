ee670fec6310bce4fffc473a41b49e6727921dbe
DA\nwuser <0ta5274738t4p4r@gmail.com>
2017-06-04 12:03:03 +0900

    画面構成

--- YDAGame's/Assets/Resources/Hasegawa/Script/GameUI.cs
+++ YDAGame's/Assets/Resources/Hasegawa/Script/GameUI.cs
@@ -6,18 +6,30 @@ using UnityEngine.UI;
 public class GameUI : MonoBehaviour {
 
 	private Text Score;
+	private Image[] Hp = new Image[5];
+	public int num;
 
 	// Use this for initialization
 	void Start () {
 		Score = GameObject.Find ("Score").GetComponent<Text> ();
+		Hp = GameObject.Find ("Hp").GetComponentsInChildren<Image> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		SetHp (num);
 	}
 
 	public void SetScore(){
 		Score.text = GameStatus.GetScore ().ToString();
+	}
+	public void SetHp(int input){
+		for (int i = 0; i < 5; i++) {
+			if (i < input) {
+				Hp [i].enabled = true;
+			} else {
+				Hp [i].enabled = false;
+			}
+		}
 	}
 }
